847eefdcca6fa293a9b32e0d81a26f52a875b12a
etokarew <etokarev@outlook.com>
2022-04-18 20:18:45 +0700

    Add opera.sh

---
+++ i3scr/opera.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+opera --new-window --password-store=basic --enable-features=OverlayScrollbar --disable-features=OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter %U
