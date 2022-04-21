6b77fb3ec8db95a8921e92bdc0499a4fa623abac
etokarew <etokarev@outlook.com>
2022-04-21 15:30:36 +0700

    Remove opera.sh

--- i3scr/opera.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-opera --new-window --password-store=basic --enable-features=OverlayScrollbar --disable-features=OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter %U
