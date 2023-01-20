ebfdc57fbc51242a935136e5685f03901b265ea4
etokarew <etokarev@outlook.com>
2023-01-20 11:58:43 +0700

    drunner.sh improvements

--- i3scr/drunner.sh
+++ i3scr/drunner.sh
@@ -1,113 +1,150 @@
 #!/bin/sh
+
 case $1 in
     autolaunch)
-        #xrandr --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
-        xrandr --output DVI-D-0 --rotate right > /dev/null &
-        xset +dpms dpms 0 0 300 > /dev/null &
-        xset s off > /dev/null &
-        picom -b > /dev/null &
-        #xrandr --listmonitors &
-        feh --bg-center /usr/src/$USER/dracula-pro/wallpapers/dracula-pro/desktop-1920x1080.png /usr/src/$USER/dracula-pro/wallpapers/dracula-pro/phone-1080x1920.png --image-bg "#282a36" > /dev/null &
-        setxkbmap -layout "us,ru" -option "grp:alt_space_toggle,grp:caps_toggle" -model "logitech" > /dev/null &
-        snixembed --proxy > /dev/null &
-        #ibus-daemon -d --replace --desktop=i3 > /dev/null &
-        dunst > /dev/null &
-        /usr/lib64/polkit-gnome/polkit-gnome-authentication-agent-1 > /dev/null &
-        gnome-keyring-daemon --daemonize > /dev/null &
-        #gnome-session-i3 --failsafe > /dev/null &
-        #gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true > /dev/null &
-        redshift-gtk -c $HOME/.config/redshift.conf > /dev/null &
-        nm-applet > /dev/null &
-        transmission-gtk --minimized > /dev/null &
-        #dropbox start -i > /dev/null &
-        solus-update-checker > /dev/null &
+        #nohup xrandr --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
+        nohup xrandr --output DVI-D-0 --rotate right &> /dev/null &
+        nohup xset +dpms dpms 0 0 300 &> /dev/null &
+        nohup xset s off &> /dev/null &
+        nohup picom -b &> /dev/null &
+        nohup #xrandr --listmonitors &
+        nohup feh --image-bg "#282a36" --bg-center \
+            /usr/src/$USER/dracula-pro/wallpapers/dracula-pro/desktop-1920x1080.png \
+            /usr/src/$USER/dracula-pro/wallpapers/dracula-pro/phone-1080x1920.png \
+            &> /dev/null &
+        nohup setxkbmap -layout "us,ru" -option "grp:alt_space_toggle,grp:caps_toggle" -model "logitech" &> /dev/null &
+        nohup snixembed --proxy &> /dev/null &
+        nohup dunst &> /dev/null &
+
+        #nohup ibus-daemon -d --replace --desktop=i3 &> /dev/null &
+        nohup /usr/lib64/polkit-gnome/polkit-gnome-authentication-agent-1 &> /dev/null &
+        nohup gnome-keyring-daemon --daemonize &> /dev/null &
+        #nohup gnome-session-i3 --failsafe &> /dev/null &
+        #nohup gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true &> /dev/null &
+        nohup nm-applet &> /dev/null &
+
+        #nohup transmission-gtk --minimized &> /dev/null &
+        nohup redshift-gtk -c $HOME/.config/redshift.conf &> /dev/null &
+        nohup copyq &> /dev/null &
+        nohup dropbox start -i &> /dev/null &
+        nohup solus-update-checker &> /dev/null &
+        nohup x0vncserver -RawKeyboard -Geometry 1920x1080 \
+            -PasswordFile /home/larry/.vnc/passwd -DisconnectClients=0 -NeverShared &> /dev/null &
         ;;
+
     calculator)
-        gnome-calculator > /dev/null
+        nohup gnome-calculator &> /dev/null &
         ;;
+
     calendar)
-        gnome-calendar > /dev/null
+        nohup gnome-calendar &> /dev/null &
         ;;
+
     clipboard-manager)
-        copyq toggle > /dev/null
+        nohup copyq toggle &> /dev/null &
         ;;
+
     downloads)
-        nautilus --new-window Downloads > /dev/null
+        nohup nautilus --new-window Downloads &> /dev/null &
         ;;
+
     dropbox-folder)
-        nautilus --new-window $HOME/Dropbox > /dev/null
+        nohup nautilus --new-window $HOME/Dropbox &> /dev/null &
         ;;
+
     dunst-clear)
-        dunstctl close-all > /dev/null
+        nohup dunstctl close-all &> /dev/null &
         ;;
+
     dunst-pause)
-        dunstctl set-paused toggle > /dev/null
+        nohup dunstctl set-paused toggle &> /dev/null &
         ;;
+
     dunst-pop)
-        dunstctl history-pop > /dev/null
+        nohup dunstctl history-pop &> /dev/null &
         ;;
+
     emoji)
-        gnome-characters > /dev/null
+        nohup gnome-characters &> /dev/null &
         ;;
+
     firefox)
-        firefox --new-window > /dev/null
+        nohup firefox --new-window &> /dev/null &
         ;;
+
     firefox-new-tab)
-        firefox --new-tab about:newtab > /dev/null
+        nohup firefox --new-tab about:newtab &> /dev/null &
         ;;
+
     firefox-private)
-        firefox --private-window > /dev/null
+        nohup firefox --private-window &> /dev/null &
         ;;
+
     i3logout)
-        i3-nagbar -t warning -m "Вы действительно хотите выйти из текущей сессии?" -b "Завершить работу i3" "i3-msg exit" > /dev/null
+        nohup i3-nagbar -t warning -m "Вы действительно хотите выйти из текущей сессии?" -b "Завершить работу i3" "i3-msg exit" &> /dev/null &
         ;;
+
     magnifier)
-        GTK_THEME=Yaru-purple-dark XCURSOR_THEME=breeze_cursors magnus --refresh-interval=100 --force-refresh > /dev/null
+        GTK_THEME=Yaru-purple-dark XCURSOR_THEME=breeze_cursors nohup magnus --refresh-interval=100 --force-refresh &> /dev/null &
         ;;
+
     recent)
-        nautilus --new-window recent\: > /dev/null
+        nohup nautilus --new-window recent\: &> /dev/null &
         ;;
+
     redshift)
         #pkill -USR1 redshift
-        redshift-gtk -x > /dev/null
+        nohup redshift-gtk -x &> /dev/null &
         ;;
+
     rofi)
-        rofi -combi-modi window,drun -show combi -modi combi > /dev/null
+        nohup rofi -combi-modi window,drun -show combi -modi combi &> /dev/null &
         ;;
+
     screenshot)
-        gnome-screenshot --interactive > /dev/null
+        nohup gnome-screenshot --interactive &> /dev/null &
         ;;
+
     settings)
-        gnome-control-center info-overview > /dev/null
+        nohup gnome-control-center info-overview &> /dev/null &
         ;;
+
     sound)
-        gnome-control-center sound > /dev/null
+        nohup gnome-control-center sound &> /dev/null &
         ;;
+
     system-monitor)
-        gnome-system-monitor -p > /dev/null
+        nohup gnome-system-monitor -p &> /dev/null &
         ;;
+
     steam)
-        steam steam://open/news > /dev/null
+        nohup steam steam://open/news &> /dev/null &
         ;;
+
     telegram)
-        telegram-desktop -- %u > /dev/null
+        nohup telegram-desktop -- %u &> /dev/null &
         ;;
+
     terminal)
-        gnome-terminal > /dev/null
+        nohup gnome-terminal &> /dev/null &
         ;;
+
     thunderbird)
-        thunderbird > /dev/null
+        nohup thunderbird &> /dev/null &
         ;;
+
     trash)
-        nautilus --new-window trash\: > /dev/null
+        nohup nautilus --new-window trash\: &> /dev/null &
         ;;
+
     updater)
-        solus-sc --update-view > /dev/null
+        nohup solus-sc --update-view &> /dev/null &
         ;;
+
     virtual-keyboard)
         echo vkbd
         ;;
+
     *)
-        echo -ne "Usage: $0 action --optional-args\n"
-        ;;
+        echo -ne "Usage: $0 action --optional-args\n" ;;
 esac
