8608599b6d3535679ff33d92d1a70d947e059c1b
Егор Токарев <33538675+etokarew@users.noreply.github.com>
2024-09-29 18:28:02 +0700

    Update drunner.sh

--- i3scr/drunner.sh
+++ i3scr/drunner.sh
@@ -81,7 +81,7 @@ case $1 in
         ;;
 
     magnifier) # snapcraft.io/magnus
-        GTK_THEME=Breeze-Dark nohup magnus --refresh-interval=100 --force-refresh &> /dev/null &
+        env GTK_THEME=Breeze-Dark XCURSOR_THEME=breeze_cursors magnus --refresh-interval=100 --force-refresh &> /dev/null &
         ;;
 
     recent)
